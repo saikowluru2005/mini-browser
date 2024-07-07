02ff6cf47dec25f6b8fc0e950370651f8232540c
saikowluru2005 <saikowluru3@gmail.com>
2024-07-07 23:04:23 +0530

    mid

--- app.py
+++ app.py
@@ -3,3 +3,10 @@ import webbrowser
 
 win=Tk()
 win.title("Mini browser")
+def search():
+    url=entry.get()
+    webbrowser.open(url)
+label1=Label(win,text="Enter URL here : ",font=("arial",10,"bold"))
+label1.grid(row=0,column=0)
+entry=Entry(win,width=30)
+entry.grid(row=0,column=1)
